e085865fad253d660a661b78170aeb5e0e15dae9
Jochen Jankowai <jochen.jankowai@liu.se>
2021-11-23 13:18:09 +0100

    Formatting

--- inviwo/opentensorvis/contourexplorer/include/inviwo/contourexplorer/algorithm/generatesegmentedtf.h
+++ inviwo/opentensorvis/contourexplorer/include/inviwo/contourexplorer/algorithm/generatesegmentedtf.h
@@ -36,9 +36,11 @@
 namespace inviwo {
 class IVW_MODULE_CONTOUREXPLORER_API SegmentationTransferFunctionGenerator {
 public:
-    static TFPrimitiveSet generateTFPrimitivesForSegments(
-        const BitSet& selection, size_t numberOfSegments, colorbrewer::Family colorFamily, double slope = 0.1,
-        const vec4& shadeColor = vec4(0.0f));
+    static TFPrimitiveSet generateTFPrimitivesForSegments(const BitSet& selection,
+                                                          size_t numberOfSegments,
+                                                          colorbrewer::Family colorFamily,
+                                                          double slope = 0.1,
+                                                          const vec4& shadeColor = vec4(0.0f));
 };
 
 }  // namespace inviwo
